d9e897c18b28382d1994ad05c89a7cc0f0266efa
Aninditaroy <anindita.roy.mu@gmail.com>
2021-03-22 12:34:36 +0600

    Added Code

---
+++ backend/data.js
@@ -0,0 +1,69 @@
+export default {
+    products: [
+   { 
+       _id: '1',
+       name:'T-shirt' ,
+       image:'/images/d1.jpg',
+       category:'T-shirt',
+       price: 20,
+       brand:'Friends Logo T-shirt',
+       rating:4.5,
+       numReviews:10,
+       countInStock: 6,
+   },
+   {
+
+        _id: '2',
+        name:'T-shirt' ,
+        image:'/images/d2.png',
+        category:'T-shirt',
+        price: 15,
+        brand:'Friends Logo T-shirt',
+        rating:3.5,
+        numReviews:10
+   },
+   {
+        _id: '3',
+        name:'Bag' ,
+        image:'/images/d3.jpg',
+        category:'Bag',
+        price: 28,
+        brand:'Friends Logo Bag',
+        rating:4.5,
+        numReviews:10,
+        countInStock: 8,
+   },
+   {
+        _id: '4',
+        name:'Bag',
+        image:'/images/d4.jpg',
+        category:'Bag',
+        price: 6.5,
+        brand:'Hand Bag',
+        rating:3.5,
+        numReviews:10
+   },
+   {
+        _id: '5',
+        name:'T-shirt' ,
+        image:'/images/d1.jpg',
+        category:'T-shirt',
+        price: 20,
+        brand:'Friends Logo T-shirt',
+        rating:4.5,
+        numReviews:10,
+        countInStock: 5,
+   },
+   {
+      _id: '6',
+      name:'T-shirt' ,
+      image:'/images/d1.jpg',
+      category:'T-shirt',
+      price: 20,
+      brand:'Friends Logo T-shirt',
+      rating:4.5,
+      numReviews:10,
+      countInStock: 5,
+  },
+  ]
+}
